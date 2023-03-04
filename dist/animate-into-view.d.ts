29b8bea1dcc75f115b3da7b61e929c999dec240f
Leesa Ward <leesa@doubleedesign.com.au>
2023-03-04 21:38:49 +1100

    Initial docs subtree commit

--- dist/animate-into-view.d.ts
+++
@@ -1,7 +0,0 @@
-interface AnimateIntoViewSettings {
-    selector: string;
-    threshold?: number;
-    type?: 'fadeIn' | 'fadeInDown' | 'fadeInUp' | 'fadeInLeft' | 'fadeInRight' | 'slideInDown' | 'slideInUp' | 'slideInLeft' | 'slideInRight';
-}
-export declare function animateIntoView(settings: AnimateIntoViewSettings): void;
-export {};
